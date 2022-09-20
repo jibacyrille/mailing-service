9646a8ebb7984564800de4639d6241a4a685a717
Cyrille Jiba <jibacyrille@gmail.com>
2022-09-20 14:40:31 +0200

    Avant profil

--- src/main/java/com/paygarde/mailing/services/EmailServiceImpl.java
+++ src/main/java/com/paygarde/mailing/services/EmailServiceImpl.java
@@ -31,7 +31,7 @@ import java.util.Map;
 import static org.thymeleaf.util.ArrayUtils.toArray;
 
 @Service
-public class EmailServiceImpl implements EmailServiceInterface{
+public class EmailServiceImpl implements EmailServiceInterface {
 
     //@Autowired
     private final Configuration configuration;
@@ -44,46 +44,37 @@ public class EmailServiceImpl implements EmailServiceInterface{
     @Value("${email.attachment.folder.path}")
     private String tempFolderPath;
 
+    @Value("${email.attachment.max.size}")
+    private String fileMaxSize;
+
     public EmailServiceImpl(Configuration configuration, JavaMailSender javaMailSender) {
         this.configuration = configuration;
         this.javaMailSender = javaMailSender;
     }
 
-    String name,username,email,mailSubject,templateName;
-    String filesUrl[];
+    String name, username, email, mailSubject, templateName;
+    String filesUrlArray[];
 
     File desFile;
-    URL url;
+    //URL url;
 
     @Override
     public void sendEmail(MailInfo mailInfo) throws MessagingException, IOException, TemplateException {
 
-        name=mailInfo.getName();
-        username=mailInfo.getUsername();
-        email=mailInfo.getEmail();
-        mailSubject=mailInfo.getMailSubject();
-        templateName=mailInfo.getTemplateName();
-        filesUrl= mailInfo.getFilesUrl();
+        name = mailInfo.getName();
+        username = mailInfo.getUsername();
+        email = mailInfo.getEmail();
+        mailSubject = mailInfo.getMailSubject();
+        templateName = mailInfo.getTemplateName();
+        filesUrlArray = mailInfo.getFilesUrl();
 
-        System.out.println(name);
-        System.out.println(username);
-        System.out.println(email);
-        System.out.println(mailSubject);
-        System.out.println(templateName);
-        System.out.println(filesUrl[0]);
-        System.out.println(Arrays.toString(filesUrl));
-        for (String s:filesUrl
-             ) {System.out.println(s);
 
-        }
+        System.out.println(Arrays.toString(filesUrlArray));
+
 
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        //MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-
-        //MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                 StandardCharsets.UTF_8.name());
-
 
         helper.setTo(mailInfo.getEmail());
         helper.setSubject(mailInfo.getMailSubject());
@@ -93,41 +84,42 @@ public class EmailServiceImpl implements EmailServiceInterface{
         configuration.getTemplate(mailInfo.getTemplateName()).process(mailInfo, stringWriter);
         String emailContent = stringWriter.getBuffer().toString();
         helper.setText(emailContent, true);
-        //helper.setText("<html><body><img src='cid:hello123'></body></html>");
+
+
+        File dossierImage = createFolder(tempFolderPath);
+
+        int attachedNumber=filesUrlArray.length;
+        if (attachedNumber!=0){
+            for (String s : filesUrlArray
+            ) {
+                if(urlIsValid(s)){
+                    File f=attachAFile(dossierImage, s, helper);
+                    double fileSize=fileSize(f);
+
+                    if(fileSize<=Double.parseDouble(fileMaxSize)){
+                        helper.addAttachment(f.getName(), f);
+                    } else{
+                        System.out.println("Le fichier "+f.getName()+" depasse la taille autorisee");
+                    }
+
+                } else {
+                    System.out.println("l'Url: "+s+" n'est pas valide");
+                }
+
+            }
+        }
 
 
 
-        //UtilMailWithAttach se = new UtilMailWithAttach(true);
-
-        File dossierImage = new File("d://images/");
-        dossierImage.mkdir();
-        System.out.println(dossierImage.getAbsolutePath());
-        try {
-                     url = new URL("https://randomuser.me/api/portraits/women/50.jpg");
-                     String extension= FilenameUtils.getExtension(url.getPath());
-                     System.out.println(extension);
-
-                     desFile=new File(dossierImage.getAbsolutePath()+"/"+FilenameUtils.getName(url.getPath()));
-                     FileUtils.copyURLToFile(url, desFile);
-                     if(desFile.exists()){
-                         helper.addAttachment(desFile.getName(),desFile);
-
-                     }
-                  } catch (MalformedURLException e) {
-                     e.printStackTrace();
-                  } catch (IOException e) {
-                     e.printStackTrace();
-                  }
-
-        try {
-            url = new URL("https://ww2.ac-poitiers.fr/ecogest/IMG/pdf/bac_pro_vente_developpement_personnel.pdf");
-            String extension= FilenameUtils.getExtension(url.getPath());
+      /*  try {
+            url = new URL("https://randomuser.me/api/portraits/women/50.jpg");
+            String extension = FilenameUtils.getExtension(url.getPath());
             System.out.println(extension);
 
-            desFile=new File(dossierImage.getAbsolutePath()+"/"+FilenameUtils.getName(url.getPath()));
+            desFile = new File(dossierImage.getAbsolutePath() + "/" + FilenameUtils.getName(url.getPath()));
             FileUtils.copyURLToFile(url, desFile);
-            if(desFile.exists()){
-                helper.addAttachment(desFile.getName(),desFile);
+            if (desFile.exists()) {
+                helper.addAttachment(desFile.getName(), desFile);
 
             }
         } catch (MalformedURLException e) {
@@ -136,26 +128,35 @@ public class EmailServiceImpl implements EmailServiceInterface{
             e.printStackTrace();
         }
 
+        try {
+            url = new URL("https://ww2.ac-poitiers.fr/ecogest/IMG/pdf/bac_pro_vente_developpement_personnel.pdf");
+            String extension = FilenameUtils.getExtension(url.getPath());
+            System.out.println(extension);
+
+            desFile = new File(dossierImage.getAbsolutePath() + "/" + FilenameUtils.getName(url.getPath()));
+            FileUtils.copyURLToFile(url, desFile);
+            if (desFile.exists()) {
+                helper.addAttachment(desFile.getName(), desFile);
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
 
         ClassPathResource pdf = new ClassPathResource("static/attachment.pdf");
         ClassPathResource image = new ClassPathResource("static/hello.jpg");
 
         FileSystemResource file = new FileSystemResource(new File("D:/hello.jpg"));
-        helper.addAttachment(file.getFilename(),file);
+        helper.addAttachment(file.getFilename(), file);
 
         helper.addAttachment("attachment.pdf", pdf);
 
         helper.addInline("hello123", image);
-       javaMailSender.send(mimeMessage);
-
-
-        boolean result = deleteFile(dossierImage);
-        if (result) {
-            System.out.println(desFile.getName() + " is deleted!");
-        } else {
-            System.out.println("Sorry, unable to delete the file.");
-        }
+        javaMailSender.send(mimeMessage);
+        deleteFile(dossierImage);
 
     }
 
@@ -168,9 +169,40 @@ public class EmailServiceImpl implements EmailServiceInterface{
         return element.delete();
     }
 
-    public File createAttachmentFolder(){
-        File dossierImage = new File(tempFolderPath);
+    public static File createFolder(String folderName) {
+        File dossierImage = new File(folderName);
         dossierImage.mkdir();
-       return dossierImage;
+        return dossierImage;
     }
+
+    public File attachAFile(File tempFolder, String fileUrl, MimeMessageHelper helper) {
+        //File dossierImage = createFolder(tempFolderPath);
+        try {
+            URL url = new URL(fileUrl);
+
+            desFile = new File(tempFolder.getAbsolutePath() + "/" + FilenameUtils.getName(url.getPath()));
+            FileUtils.copyURLToFile(url, desFile);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return desFile;
+    }
+
+    public boolean urlIsValid(String url) {
+        try {
+            (new java.net.URL(url)).openStream().close();
+            return true;
+        } catch (Exception ex) { }
+        return false;
+    }
+
+    public double fileSize(File f){
+        int temp=1024*1024;
+        return (f.length()/temp);
+    }
+
+
 }
